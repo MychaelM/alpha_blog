3c33005776bfd8fc5128e0bb109d6f3bc0d175c5
Mychael M <mychaelbmenges@gmail.com>
2021-07-14 21:27:28 -0600

    rolled back initial db migration to add missing field and re migrated db to initialize articles table

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_032000) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
