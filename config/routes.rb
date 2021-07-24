ae76493a710f2ae05190f183dec94d3e15ea853a
Mychael M <mychaelbmenges@gmail.com>
2021-07-24 13:44:13 -0600

    added user routes, updated shared errors partial, and added user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
+  resources :users, except: [:new] #already have route defined for new users at 'signup' so exclides new route from user resource
 end
